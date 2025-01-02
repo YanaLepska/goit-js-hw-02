28511902eb5b98b458acd8575d972cee6b241159
YanaLepska <lepskayana1624@gmail.com>
2025-01-02 23:12:06 +0200

    Level-4-3-tasks

--- js/level4/level-4-3.js
+++ js/level4/level-4-3.js
@@ -1,10 +1,58 @@
 console.log("Task-4-1");
-
+const delDouble = array => {
+    let newArray = [];
+    for (const element of array) {
+        if (!newArray.includes(element)) {
+            newArray.push(element);
+        }
+    }
+    return newArray;
+};
+console.log(delDouble([15, 298, 35, 74, 19, 15, 74]));
 //-------------------------------------------------------------------------------------
 console.log("Task-4-2");
+console.log();
+
+const delDoubleTwo = array => {
+    let newArray = [];
+    let count = {};
+ for (let i = 0; i < array.length; i++) {
+     count[array[i]] = (count[array[i]] || 0) + 1;
+    }
+    for (let i = 0; i < array.length; i++) {
+
+        if (count[array[i]]<=3) {
+             newArray.push(array[i]);
+         }
+        
+    }
+    console.log(count);
+    return newArray;
+};
+
+console.log(delDoubleTwo([11,32,95,57,32,57,57,32,11,99,57,117]));
 
 //-------------------------------------------------------------------------------------
 console.log("Task-4-3");
+const delDoubles = array => {
+    let newArray = [];
+    for (let i = 0; i < array.length; i++) {
+        if (array[i]!==array[i+1] && array[i]!==array[i-1]) {
+            newArray.push(array[i]);
+        }
+        
+    }
+    return newArray;
+};
+console.log(delDoubles([11,32,32,95,57,32,57,57,32,32,99,117]));
 
 //-------------------------------------------------------------------------------------
 console.log("Task-4-4");
+
+const extremums = array => {
+    const max = Math.max(...array);
+    const min = Math.min(...array);
+    return { max, min };
+
+};
+console.log(extremums([11,32,8,95,57,32,57,89,132,65,99,17]));
